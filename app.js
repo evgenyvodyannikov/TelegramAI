f47279be1e2a59882e5c9e38b598beb536f60095
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2023-04-01 20:55:49 +0500

    use chatgpt client

--- app.js
+++ app.js
@@ -1,27 +1,106 @@
-import dotenv from 'dotenv'
-import TelegramBot from 'node-telegram-bot-api';
+import dotenv from "dotenv";
+import TelegramBot from "node-telegram-bot-api";
+import ChatGPTClient from "@waylaidwanderer/chatgpt-api";
+
 dotenv.config();
 
 // Env Variables
-const token = process.env.TelegramToken;
-const openaiApiKey = process.env.openaiApiKey;
-const adminId = process.env.AdminId;
+const telegramToken = process.env.TELEGRAM_BOT_TOKEN;
+const accessToken = process.env.OPENAI_ACCESS_TOKEN;
+const telegramAdminId = process.env.TELEGRAM_ADMIN_ID;
+
+const clientOptions = {
+  modelOptions: {
+    model: "gpt-3.5-turbo",
+    temperature: 0,
+  },
+  // (Optional) Set custom instructions instead of "You are ChatGPT...".
+  // promptPrefix: 'You are Bob, a cowboy in Western times...',
+  // (Optional) Set a custom name for the user
+  // userLabel: 'User',
+  // (Optional) Set a custom name for ChatGPT
+  // chatGptLabel: 'ChatGPT',
+  // (Optional) Set to true to enable `console.debug()` logging
+  debug: false,
+};
+
+const cacheOptions = {};
+
+const chatGptClient = new ChatGPTClient(
+  accessToken,
+  clientOptions,
+  cacheOptions
+);
+
+let response;
 
 // Create the bot
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(telegramToken, { polling: true });
 
 // Message Handlers
 
-bot.on('message', (msg) => {
-
-    let response = 'Received your message';
-    if(msg.text == 'Penis' || msg.text == 'Пенис'){
-        response = 'Да я люблю сосать член, это пенис'
-    }
+// Matches "/echo [whatever]"
+bot.onText(/\/c (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
 
   const chatId = msg.chat.id;
+  if(chatId == telegramAdminId){
+        const resp = match[1];
 
-  bot.sendMessage(chatId, response);
+    // send back the matched "whatever" to the chat
+        bot.sendMessage(chatId, resp);
+  }
 
 });
 
+bot.on("message", async (msg) => {
+
+  if (response) {
+    response = await chatGptClient.sendMessage(msg.text, {
+      conversationId: response.conversationId,
+      parentMessageId: response.messageId,
+    });
+  }
+  else{
+    response = await chatGptClient.sendMessage(msg.text);
+  }
+
+  console.log(response);
+  console.log(msg)
+
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, response.response);
+});
+
+// response = await chatGptClient.sendMessage("Hello!");
+// console.log(response); // { response: 'Hello! How can I assist you today?', conversationId: '...', messageId: '...' }
+
+// response = await chatGptClient.sendMessage("Write a short poem about cats.", {
+//   conversationId: response.conversationId,
+//   parentMessageId: response.messageId,
+// });
+// console.log(response.response); // Soft and sleek, with eyes that gleam,\nCats are creatures of grace supreme.\n...
+// console.log();
+
+// response = await chatGptClient.sendMessage("Now write it in French.", {
+//   conversationId: response.conversationId,
+//   parentMessageId: response.messageId,
+//   // If you want streamed responses, you can set the `onProgress` callback to receive the response as it's generated.
+//   // You will receive one token at a time, so you will need to concatenate them yourself.
+//   onProgress: (token) => process.stdout.write(token),
+// });
+// console.log();
+// console.log(response.response); // Doux et élégant, avec des yeux qui brillent,\nLes chats sont des créatures de grâce suprême.\n...
+
+// response = await chatGptClient.sendMessage("Repeat my 2nd message verbatim.", {
+//   conversationId: response.conversationId,
+//   parentMessageId: response.messageId,
+//   // If you want streamed responses, you can set the `onProgress` callback to receive the response as it's generated.
+//   // You will receive one token at a time, so you will need to concatenate them yourself.
+//   onProgress: (token) => process.stdout.write(token),
+// });
+// console.log();
+// console.log(response.response); // "Write a short poem about cats."
