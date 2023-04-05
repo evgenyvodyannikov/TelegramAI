d39a6069cf18a82dd1046b0ce7c9f0ed3f21b161
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2023-04-06 01:11:32 +0500

    create /cat command that return some cat image

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const bot = new TelegramBot(telegramToken, { polling: true });
 bot.onText(/\/reset/, (msg) => {
   const chatId = msg.chat.id;
   if (checkPesmission(chatId)) {
-    responses.filter(response => response.ChatId == chatId)[0].Data = null;
+    responses.filter((response) => response.ChatId == chatId)[0].Data = null;
     bot.sendMessage(chatId, "Dialog was reset successfully!");
   }
 });
@@ -70,25 +70,24 @@ bot.onText(/\/sendMsg (.+)/, async (msg, match) => {
     let userId = query.substring(0, query.indexOf("|"));
     let message = query.substring(query.indexOf("|") + 1, query.length).trim();
     bot.sendMessage(telegramAdminId, "Generating response");
-    let resData =  await chatGptClient.sendMessage(message);
+    let resData = await chatGptClient.sendMessage(message);
     bot.sendMessage(userId, resData.response);
     bot.sendMessage(telegramAdminId, "Success.");
     isReady = true;
   }
 });
 
-bot.onText(/\/sendpic/, (msg) => {
-  bot.sendPhoto(
-    msg.chat.id,
-    "https://sun9-77.userapi.com/impg/klcqWtISQK1oxuoR7aIK9gEP2ET82hYGyP0NwQ/xBIyFUcdg5o.jpg?size=397x470&quality=95&sign=4d5c5e1fa23583bb4e5805c0739231db&type=album",
-    { caption: "Бля! \nЯ со смены! " }
-  );
+bot.onText(/\/cat/, (msg) => {
+  let url = `https://cataas.com/cat?${Date.now()}`; // To avoid Telegram sending the same picture because url the same
+  bot.sendPhoto(msg.chat.id, url, {
+    caption: "Here is your random cat!"
+  });
 });
 
 bot.onText(/\/keyboard/, (msg) => {
   bot.sendMessage(msg.chat.id, "Welcome", {
     reply_markup: {
-      keyboard: [["/sendpic", "Hello, ChatGPT!"], ["/keyboard"]],
+      keyboard: [["/reset", "/cat"], ["/keyboard"]],
     },
   });
 });
@@ -102,7 +101,9 @@ bot.on("message", async (msg) => {
       .then((result) => (loadingMsgId = result.message_id));
     isReady = false;
 
-    let currentUserRes = responses.filter(response => response.ChatId == chatId)[0];
+    let currentUserRes = responses.filter(
+      (response) => response.ChatId == chatId
+    )[0];
     if (currentUserRes?.Data) {
       currentUserRes.Data = await chatGptClient.sendMessage(msg.text, {
         conversationId: currentUserRes.Data.conversationId,
@@ -110,12 +111,15 @@ bot.on("message", async (msg) => {
       });
     } else {
       let newRes = await chatGptClient.sendMessage(msg.text);
-      let index = responses.push({ChatId: chatId, Data: newRes});
+      let index = responses.push({ ChatId: chatId, Data: newRes });
       currentUserRes = responses[index - 1];
     }
     bot.deleteMessage(chatId, loadingMsgId);
+    console.log(currentUserRes.Data);
     //bot.editMessageText(response.response, {chat_id: chatId, message_id: loadingMsgId})
-    bot.sendMessage(chatId, currentUserRes.Data.response);
+    bot.sendMessage(chatId, currentUserRes.Data.response, {
+      parse_mode: "Markdown",
+    });
     isReady = true;
   }
 });
